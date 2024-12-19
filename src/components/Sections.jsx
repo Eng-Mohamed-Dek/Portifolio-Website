dddf73c4605465311f6acc9876551b611dc26481
mohameddek <mohameddek366@gmail.com>
2024-12-18 22:20:39 -0500

    text width

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -4,7 +4,7 @@ const Sections = ({ title, subtitle, desc , icon}) => {
   return (
     <div>
       <h1 className='text-center md:text-4xl text-2xl font-bold mb-5'>{title}<span className='font-light'> {subtitle}</span></h1>
-      <p className='text-gray-500 mb-16 text-center w-[55%] mx-auto'>{desc}
+      <p className='text-gray-500 mb-16 text-center md:w-[55%] w-[95%] mx-auto'>{desc}
       </p>
       <a
         href="https://github.com/Eng-Mohamed-Dek"
